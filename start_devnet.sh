6af79d8cc554223fd9774bd5d314155fe5f05e34
Andreas Bigger <abigger87@gmail.com>
2023-03-31 21:41:01 -0400

    add devnet down to startup

--- start_devnet.sh
+++ start_devnet.sh
@@ -7,7 +7,7 @@ source .env.devnet
 MONOREPO_DIR="$HOME/dev/optimism/monorepo"
 
 # Boot up the devnet
-(cd $MONOREPO_DIR && make devnet-up)
+(cd $MONOREPO_DIR && make devnet-down && make devnet-up)
 
 # Deploy the mock dispute game contract
 (cd ./testdata/mock-dgf && forge script script/DeployMocks.s.sol --rpc-url http://localhost:8545 --private-key $OP_CHALLENGER_KEY --broadcast)
